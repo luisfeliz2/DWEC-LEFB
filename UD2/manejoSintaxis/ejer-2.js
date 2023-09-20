67421ddcd4cb664233dc6a50a78bd5dc2ca1d360
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-09-20 12:34:28 +0200

    20-09-2023 continuando manejo sintaxis

---
+++ UD2/manejoSintaxis/ejer-2.js
@@ -0,0 +1,14 @@
+let nombre = prompt("nombre");
+let apellido = prompt("apellidos");
+let valor1 = parseInt( prompt("valor 1"));
+let valor2 =parseInt( prompt("vlor 2"));
+let valor3 = parseInt(prompt("valor  3"));
+
+let suma = valor1+valor2;
+console.log(
+    `tu nombre es ${nombre} ${apellido}
+    los valores son ${valor1} y ${valor2}
+    el resultado es ${suma}
+    `
+)
+//${}
---
+++ UD2/manejoSintaxis/ejer-5.js
@@ -0,0 +1,13 @@
+
+sueldo = parseInt(prompt("introduce sueldo"))
+antiguedad = parseInt(prompt("introduce antiguedad"))
+
+if(sueldo >500 && antiguedad){
+    sueldo = sueldo*3
+
+}
+
+console.log(`
+Entrada     salida
+${sueldo}   ${sueldo}
+`)
