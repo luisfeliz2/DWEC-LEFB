02f0a5f28365f86280b5d8c84f080d5bd7204d47
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-09-25 12:39:08 +0200

    hola

--- UD2/manejoSintaxis/ejer-5.js
+++ UD2/manejoSintaxis/ejer-5.js
@@ -1,13 +1,14 @@
 
-sueldo = parseInt(prompt("introduce sueldo"))
-antiguedad = parseInt(prompt("introduce antiguedad"))
-
-if(sueldo >500 && antiguedad){
-    sueldo = sueldo*3
+let sueldo = parseInt(prompt("introduce sueldo"))
+let antiguedad = parseInt(prompt("introduce antiguedad"))
+let nuevoSueldo = sueldo;
+if(sueldo >500 && antiguedad > 10 ){
+    nuevoSueldo = sueldo*3
 
 }
 
 console.log(`
 Entrada     salida
-${sueldo}   ${sueldo}
+${nuevoSueldosueldo
+    ,antiguedad}   ${sueldo}
 `)
