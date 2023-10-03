71c7b8cec239c836dc9564a339e19290ee371487
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-10-03 13:27:40 +0200

    slumnos cons su notas

---
+++ UD3/ejer-4-2.js
@@ -0,0 +1,10 @@
+
+// Crear un script en el que se pregunte por el número de alumnos y en función de este dato
+// pida el nombre de los alumnos y sus notas finales. Al final se debe mostrar, cada alumno con su nota
+// y después la media de todos los alumnos.
+
+
+let numAlumnos = parseInt( prompt("dime el numero de alumnos"));
+
+
+let arrayAlum = new Array(numAlumnos);
