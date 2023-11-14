b6fc3e1d58d685686fa93f7eee4e9106235aa6e5
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-11-14 12:17:03 +0100

    validar eddad nombre etc

--- UD5/Enunciado/eventos-ini.js
+++ UD5/Enunciado/eventos-ini.js
@@ -3,19 +3,77 @@ var color_css = 'background: #222; color: #bada55'
 * Uso de addEventListener this y Event
 **/
 
- document.getElementById("idEnviar").addEventListener("click", mostrarInformacion);
+let boton = document.getElementById("idEnviar")
+boton.addEventListener("click", validarInformacion);
+
+let formulario=document.getElementById("idFormulario")
 
 
-function mostrarInformacion(e){
-    
-    console.clear()
-    console.log(`Usando this : %c${this.value}`,color_css);
-    console.log(`Usando Event : %c${e.target.value}`,color_css);
-    console.log(e)
-    
-    e.preventDefault();
+function validarInformacion(e){    
 
-    // para que no se envie a otra pagina
+
+     this.disabled = true;
+   
+    if(validarAPIHTML(e) && validarJS(e) && confirm("¿Deseas enviar el formulario?")){
+        
+        return true;
+
+    }else{
+        e.preventDefault();
+        this.disabled = false;
+        return false;
+    }    
 }
 
+function validarAPIHTML(e){
 
+    console.log("Pendiente de implementar")
+    
+    return true
+}
+
+function validarJS(e){
+    
+    return validarNombre() && validarEdad()
+}
+
+function validarEdad(){
+    let zonaError =  document.getElementById("idErrorNombre")
+    let edad = formulario["idEdad"].value
+
+    if(isNaN(edad)){
+        zonaError.innerHTML="Especifica un valor numerico"
+    }
+
+    if(edad >=18 || edad <= 99){
+        zonaError.innerHTML="Tamaño incorrecto"
+        return false
+    }
+
+    //console.log(formulario["idEdad"])
+    console.log("Pendiente de programar, validar Edad")
+    return true
+}
+
+function validarNombre(){
+    let nombre = formulario["idNombre"].value.trim()
+    let zonaError= document.getElementById("idErrorNombre")
+    let mensajeNombre = nombre.length > 10 || nombre.length <5 ? "tamaño incorggrecto..." : ""
+    console.log(mensajeNombre)
+    if(nombre === ""){
+        
+        zonaError.innerHTML="campo Vacio"
+        return false;
+    }
+    
+    if (nombre.length  > 30 || nombre.length <5){  
+        zonaError.innerHTML="tamaño incorrecto..."
+        return false;
+    }
+    
+
+
+
+    console.log("Pendiente de programar, validar nombre")
+    return true
+}
