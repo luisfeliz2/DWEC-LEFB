62e21f46267161994db099dcf6b010e865567ad3
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-11-28 10:16:14 +0100

    validacion api html

--- UD5/Ejercicio Demo Formularios-20231122/Ejercicio-21-Nov/javascript.js
+++ UD5/Ejercicio Demo Formularios-20231122/Ejercicio-21-Nov/javascript.js
@@ -1,8 +1,8 @@
 /*hola*/
-alert("hola")
+
 // Al hacer click en el botón de enviar tendrá que llamar a la la función validar 
 // que se encargará de validar el formulario.
-document.getElementById("idEnviar").addEventListener('click', validar, false);
+document.getElementById("idEnviar").addEventListener('click', validar);
 let formulario=document.forms["miFormulario"]
 
 /****************************************************************
@@ -11,9 +11,10 @@ let formulario=document.forms["miFormulario"]
  *          - Mediante JS
  *          - Mediante Api de acceso a validación de html
  ****************************************************************/
-function validar(e){    
+function validar(e){
     // IMPORTANTE!!! Realizar limpieza del formulario, a nivel de ClassName y CustomMessage
-    for (let i=0; i<formulario.elements.length; i++){
+    //limpieza
+    for (let i=0; i<formulario.length; i++){
         formulario.elements[i].classList.remove("errorInput")
         formulario.elements[i].setCustomValidity("")
     }
@@ -23,9 +24,9 @@ function validar(e){
         
     // IMPORTANTE!!! Deshabilitamos el botón
     this.disabled = true;
+    console.log("llega hasta aqui")
 
-
-    if(validarAPIHTML(e) && (e) && confirm("¿Deseas enviar el formulario?")){           
+    if(validarAPIHTML(e) && validarJS(e) && confirm("¿Deseas enviar el formulario?")){           
         return true;
 
     }else{
@@ -41,7 +42,48 @@ function validar(e){
  function validarJS(eventopordefecto) {
     
     // Validamos cada uno de los apartados con llamadas a sus funciones correspondientes.
-    return validarEdad() // && validarXXXXX();             
+    return validarEdad() && validarJSInteres()// && validarCorreo();          
+    
+    
+}
+function validarEdad(){
+    return true
+}
+function validarJSInteres(params) {
+    
+    let interes = new Set(["deportes","musica","viaje","y-mas"]);
+    let valorInteres = formulario.querySelectorAll("input[type='checkbox']")
+    let valorSpam = document.getElementById("idInteresesError")
+
+
+    // let valorInteres2= formulario.querySelectorAll("input[name='interes']")
+    // let valorInteres3 = formulario["interes"];
+
+    let selecionado = 0;
+    let error="";
+    valorInteres.forEach(chechk =>{
+        if (chechk.checked ===true) {
+            selecionado += 1
+            if (interes.has(chechk.value)===false) {
+                error +=`El valor; ${chechk.value},no existe; `
+            }
+        }
+    })
+
+    if (selecionado===0) {
+        valorSpam.innerHTML="Debes selecionar un interes....."
+        return false
+        
+    }
+
+    if (error!=="") {
+
+        valorSpam.innerHTML=error
+        console.log(error)
+        return false
+    }
+
+    return true
 }
 
 /*********************************************************************************************
@@ -49,7 +91,7 @@ function validar(e){
  ********************************************************************************************/
  function validarAPIHTML(eventopordefecto) {
     
-    return validarNombreAPIHTML() //&& validarXXXXAPIHTML;            
+    return validarNombreAPIHTML() && validarEdadAPIHTML();            
 
 }
 
@@ -57,6 +99,48 @@ function validar(e){
 /***************************************************************************/
 /************************FUNCIONES INDIVIDUALES*****************************/
 /***************************************************************************/
+
+function validarEdadAPIHTML(params) {
+    
+    let inputEdad = formulario["idEdad"];
+    let span = document.getElementById("idEdadError")
+
+
+    if (inputEdad.validity.valueMissing){
+
+        let mensajeError ="Campo obligatorio"
+        inputEdad.focus()
+        inputEdad.setCustomValidity(mensajeError)
+        span.innerHTML=inputEdad.validationMessage
+
+    }else if(inputEdad.validity.rangeUnderflow){
+
+        let mensajeError ="valor debe debe ser mayor de 18"
+        inputEdad.focus()
+        inputEdad.setCustomValidity(mensajeError)
+        span.innerHTML=inputEdad.validationMessage
+    }else if(inputEdad.validity.rangeOverflow){
+
+        let mensajeError ="valor debe debe ser menor de 100"
+        inputEdad.focus()
+        inputEdad.setCustomValidity(mensajeError)
+        span.innerHTML=inputEdad.validationMessage
+        
+    }
+
+
+
+
+
+    return false
+
+
+
+
+}
+
+
+
 
 function validarNombreAPIHTML() {
     
