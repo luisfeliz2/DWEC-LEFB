c1232190f4dcbcdf913aceb674652216bddc852a
luisfeliz2 <luisfelizbeltre@gmail.com>
2024-01-22 14:19:38 +0100

    dsds

--- vuejs/fomulario/src/components/Inicio.vue
+++ vuejs/fomulario/src/components/Inicio.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, reactive } from 'vue'
+import { computed, reactive, watch } from 'vue'
 import { ref } from 'vue'
 
 defineProps({
@@ -9,8 +9,54 @@ defineProps({
   }
 })
 
-let base = ref("")
-let altura= ref("")
+let base = ref(0)
+let altura= ref(0)
+let largo=ref(0)
+let ancho=ref(0)
+let alto = ref(0)
+const mensaje=reactive({
+  mensaje_alto:"",
+  mensaje_ancho:"",
+  mensaje_largo:""
+})
+let modificado =false;
+
+watch(alto,(nuevoValor,antiguoValor)=>{
+  if (isNaN(nuevoValor)) {
+
+   
+   
+    mensaje.mensaje_alto=`valor incorrecto ${nuevoValor}`
+    alto.value=antiguoValor
+    modificado=true
+  }else{
+   if(modificado==true){
+    modificado=false
+   }else{
+    mensaje.mensaje_alto=""
+   }
+  }
+  
+ 
+   
+  
+
+  
+})
+watch(ancho,(nuevoValor,antiguoValor)=>{
+  if (isNaN(nuevoValor)) {
+    ancho.value=antiguoValor
+  }else{
+
+  }
+})
+watch(largo,(nuevoValor,antiguoValor)=>{
+  if (isNaN(nuevoValor)) {
+    largo.value=antiguoValor
+  }else{
+
+  }
+})
 
 function limpiar(params) {
   base.value=""
@@ -18,36 +64,64 @@ function limpiar(params) {
 }
 
 const area = computed(function luis() {
+  if (ancho.value >0) {
+    
+  }
   return base.value*altura.value
   
 })
 
-const perimetro = computed()
+const perimetro = computed(()=>2*ancho.value + 2*alto.value);
 
+const volumen = computed(()=> ancho.value*largo.value*alto.value)
+//se ejecuta cuendo cambie el valor de las variabñes
+mostrarElemento=false
+function mostrarElemento(params) {
+  if (ancho.value >0) {
+    mostrarElemento =!mostrarElemento 
 
-
+    
+  }
+}
 </script>
 
 <template>
   <div class="greetings">
     <form @submit.prevent="">
-      <label for="">base
+      <label for="">largo
+        <span>{{ mensaje.mensaje_largo }}</span>
+        <input v-model="largo">
 
-        <input v-model="base">
-      </label>
-      <label for="">altura
-
-        <input v-model="altura">
+      
       </label>
 
+      <label for="">alto
+        <input v-model="alto">
+        <span>{{ mensaje.mensaje_alto }}</span>
+        <br>
+      </label>
+
+
+      <label for="">ancho
+        <input @click="mostrarElemento2" v-model="ancho">
+        <span>{{ mensaje.mensaje_ancho }}</span>
+      </label>
 
 
 
 
     </form>
 
-    <p>Area : {{ area }} </p>
-    <p>Perimetro : {{ altura }} </p>
+<div v-show="mostrarElemento" >
+  
+  <p>Area : {{ area }} </p>
+    
+    <p>Perimetro : {{ perimetro }} </p>
+    <p>volumen : {{ volumen }} </p>
+
+</div>
+  
+
   </div>
 </template>
 
